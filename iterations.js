79382f2daad0abc1f1a2b5b3411e73850e5f5874
showermax <sevenhauz@gmail.com>
2023-02-04 13:46:01 +0300

    Create iterations.js

---
+++ iterations.js
@@ -0,0 +1,15 @@
+// Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+// Setup
+const myArray = [];
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// Only change code below this li// Setup
+const myArray = [];
+for (let x = 1; x < 6; x++) {
+  myArray.push(x);
+}
+// Only change code below this linene
