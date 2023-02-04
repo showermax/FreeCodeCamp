2a51d7ba533ee37cdf1caebf5877c02671f616ca
showermax <sevenhauz@gmail.com>
2023-02-04 13:51:49 +0300

    Update iterations.js

--- iterations.js
+++ iterations.js
@@ -19,3 +19,10 @@ for (let i = 1; i < 10; i+=2){
   myArray.push(i);
 }
 // Only change code below this line
+
+// Setup
+const myArray = [];
+for (let i = 9; i > 0; i-=2) {
+  myArray.push(i);
+}
+// Only change code below this line
