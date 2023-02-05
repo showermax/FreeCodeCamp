2594abaa9523467d66dcd6575fb6ecd1a2b55b5c
showermax <sevenhauz@gmail.com>
2023-02-05 21:05:19 +0300

    2

--- the Conditional (Ternary) Operator.js
+++ the Conditional (Ternary) Operator.js
@@ -4,3 +4,11 @@ function checkEqual(a, b) {
    }
    
    checkEqual(1, 2);
+//n the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return positive, negative or zero.
+
+function checkSign(num) {
+    return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero"; 
+  }
+  
+  checkSign(10);
+  console.log(checkSign(-4));
