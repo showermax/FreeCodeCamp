3a6107224a92b043d1ac16582c5db65765fb72ed
showermax <sevenhauz@gmail.com>
2023-02-05 18:47:20 +0300

    Update iterations.js

--- iterations.js
+++ iterations.js
@@ -48,3 +48,14 @@ function multiplyAll(arr) {
   }
   
   multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+ // Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
+  // Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
