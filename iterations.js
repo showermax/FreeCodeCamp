acb22965d57716a386755035d330b856bb064573
showermax <sevenhauz@gmail.com>
2023-02-05 20:38:14 +0300

    Update iterations.js

--- iterations.js
+++ iterations.js
@@ -111,6 +111,34 @@ const contacts = [
   },
 ];
 
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
 function lookUpProfile(name, prop) {
   // Only change code below this line
 for (let i = 0; i < contacts.length; i++) {
@@ -119,11 +147,11 @@ for (let i = 0; i < contacts.length; i++) {
       return contacts[i][prop]; }
       else {return "No such property";}
 } 
+}
 return "No such contact";
-  }
 }
 
   // Only change code above this line
 
 lookUpProfile("Akira", "likes");
-console.log(lookUpProfile("Akira", "likes"));
+console.log(lookUpProfile("Kristian", "likes"));
