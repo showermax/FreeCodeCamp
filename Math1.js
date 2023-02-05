8e3eeb19921d5223efb1ba228bc1f93afa4db962
showermax <sevenhauz@gmail.com>
2023-02-05 20:49:42 +0300

    Update Math1.js

--- Math1.js
+++ Math1.js
@@ -8,3 +8,17 @@ function randomFraction() {
     // Only change code above this line
   }
   console.log(randomFraction());
+
+  function randomWholeNum() {
+
+    // Only change code below this line
+  
+    return Math.floor(Math.random()*10);
+  }
+
+ // Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+ function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // Only change code above this line
+}
