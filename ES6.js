c16d2e6e53486b1e5a1841502638df506dad2f57
showermax <sevenhauz@gmail.com>
2023-02-08 12:08:39 +0300

    v2

--- ES6.js
+++ ES6.js
@@ -1,3 +1,12 @@
 const doubler = item => item * 2;
 const multiplier = (item, multi) => item * multi;
 multiplier(4, 2);
+
+var myConcat = function(arr1, arr2) {
+    return arr1.concat(arr2);
+  };
+  // Arrow Functions
+  const myConcat1 = (arr1, arr2) => arr1.concat(arr2);
+  console.log(myConcat([1, 2], [3, 4, 5]));
+  console.log(myConcat1([1, 2], [3, 4, 5]));
+  
