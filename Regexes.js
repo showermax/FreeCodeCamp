daa08f11e9c8e73e6c83a06a118f532881e00d8e
showermax <sevenhauz@gmail.com>
2023-02-16 16:49:47 +0300

    6

--- Regexes.js
+++ Regexes.js
@@ -69,6 +69,6 @@ console.log(result);
  myRegex = /[h-s2-6]/gi; // Change this line
  result = quoteSample.match(myRegex); // Change this line
 
- let quoteSample = "3 blind mice.";
+let quoteSample = "3 blind mice.";
 let myRegex = /[^aeiou1-9]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
